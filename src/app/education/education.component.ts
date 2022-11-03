f2362c2005521903ec8b9eb142e7db232a73bae7
Karl Roxas <supercrane21@protonmail.com>
2022-11-03 11:18:38 +0800

    changes in education

--- src/app/education/education.component.ts
+++ src/app/education/education.component.ts
@@ -12,4 +12,9 @@ export class EducationComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  //Highlight Text
+  hText : string = "At NU, we revel in a culture of learning by doing. In";
+  hTextLink : string = " 30 departments across five schools and one college";
+  hText2 : string = ", our students combine analytical rigor with curiosity, playful imagination, and an apetite for solving the hardest problems in";
+  hTextLink2 : string = " service to society.";
 }
